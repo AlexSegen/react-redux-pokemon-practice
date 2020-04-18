0c1924bfc9feb8ef5e3274e8abf7d5a1d76ea502
Alejandro Vivas <alejandrojv@gmail.com>
2020-04-17 23:35:14 -0400

    Make fetch data

--- src/components/buscador/BuscadorPokemon.jsx
+++ src/components/buscador/BuscadorPokemon.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import fetchPokemon from "../../store/actions/buscadorAction";
 
 const BuscadorPokemon = () => {
-    return (
-        <div className="form-group">
-            <input className="form-control" type="text"/>
-            <button type="button" className="btn btn-primary m-3">Buscar</button>
-        </div>
-    )
-}
+  const dispatch = useDispatch();
+  const [pokemonName, setPokemonName] = useState("");
+  
+  const searchPokemon = () => {
+      dispatch(fetchPokemon(pokemonName))
+  }
+
+  return (
+    <div className="form-group">
+     <input
+        className="form-control"
+        type="text"
+        onChange={(e) => setPokemonName(e.target.value)}
+      />
+       <button
+        type="button"
+        className="btn btn-primary m-3"
+        onClick={() => searchPokemon()}
+        
+      >
+        Buscar
+      </button>
+    </div>
+  );
+};
 
 export default BuscadorPokemon;
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducers from "./reducers/rootReducers";
 import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducers, composeWithDevTools());
+const store = createStore(rootReducers, composeWithDevTools(
+    applyMiddleware(thunk)
+));
 
 export default store;
